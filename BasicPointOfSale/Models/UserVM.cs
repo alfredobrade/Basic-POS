b411fbee5ea5c1d226a08b0eddc1f9df628dde06
Alfredo Nicolás Brade <alfredobrade@gmail.com>
2023-07-28 10:08:36 -0300

    ready to deploy

--- BasicPointOfSale/Models/UserVM.cs
+++ BasicPointOfSale/Models/UserVM.cs
@@ -5,7 +5,7 @@ namespace BasicPointOfSale.Models
     public class UserVM
     {
         public int? Id { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
         public string? LastName { get; set; }
         public string Email { get; set; }
         public string? Password { get; set; }
