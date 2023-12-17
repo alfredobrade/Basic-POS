058519321f991c1ecb36663cdfa15869356406dc
alfredobrade <alfredobrade@gmail.com>
2023-12-17 00:25:33 +0000

    DBScripts had been created

---
+++ DBScripts/ContextConection.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PointOfSale.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBScripts
+{
+    public class ContextConection
+    {
+        private readonly POSContext _context;
+        private int _index;
+
+        public ContextConection(POSContext context)
+        {
+            _context = context;
+        }
+
+        //TODO: metodo donde se van a realizar todos los updates necesarios
+        public int DBUpdates()
+        {
+            try
+            {
+                //init index
+                _index = 0;
+
+                //procesos a realizar
+
+
+
+                //exposicion de la cantidad de modificaciones realizadas
+                return _index;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
+
+        //modificacion de Sales
+        public async Task SalesDBUpdate()
+        {
+            try
+            {
+                var list = _context.Sales.AsQueryable();
+                foreach (var item in list)
+                {
+                    //se controla si el CustomerId es null
+                    if (item.CustomerId == null)
+                    {
+                        item.CustomerId = 0;
+                        _context.Sales.Update(item);
+                        _index++;
+                        Console.WriteLine($"SaleId {item.Id} Update CustomerId {item.CustomerId}");
+                    }
+                    //las ventas sin fecha no se eliminan porque son ventas abiertas
+                    //if (item.DateTime == null)
+                    //{
+                    //    Console.WriteLine($"SaleId {item.Id} Update CustomerId {item.CustomerId}");
+
+                    //    _context.Sales.Remove(item);
+                    //    _index++;
+                    //}
+
+                }
+                await _context.SaveChangesAsync();
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ DBScripts/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DBScripts;
+using Microsoft.EntityFrameworkCore;
+using PointOfSale.DAL.Context;
+
+Console.WriteLine("Hello, World!");
+//string n = Console.ReadLine();
+
+//for (int i = 0; i < int.Parse(n); i++)
+//{
+//    Console.WriteLine("Hello, World!" + i);
+
+
+//}
+
+
+//services.AddDbContext<POSContext>(options => options.UseNpgsql(configuration.GetConnectionString("DevConnection")));
+
+//var _context = new POSContext();
+
+
+// Configura las opciones de DbContext, reemplaza YourDbContextOptions con el tipo real
+DbContextOptions<POSContext> dbContextOptions = new DbContextOptionsBuilder<POSContext>()
+    .UseNpgsql("Server=bboawflhkcoz33rnv6js-postgresql.services.clever-cloud.com;Port=50013;Database=bboawflhkcoz33rnv6js;User Id=u0hjnypgj4c6muk7kf2n;Password=KqWZndvqFcwA7w4Th9fJ59cPRAjdyE;") // Reemplaza con tu cadena de conexión PostgreSQL
+    .Options;
+
+// Crea una instancia de POSContext pasando las opciones al constructor
+using (POSContext context = new POSContext(dbContextOptions))
+{
+    // Crea una instancia de ContextConection utilizando POSContext
+    ContextConection contextConnection = new ContextConection(context);
+
+    // Llama al método SalesDBUpdate
+    await contextConnection.SalesDBUpdate();
+
+    // Haz algo con el resultado si es necesario
+    //Console.WriteLine($"Se realizaron {actualizaciones} actualizaciones en la base de datos.");
+}
+Console.ReadLine();
